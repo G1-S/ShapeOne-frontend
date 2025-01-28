e8d01a29152396fa8bf14c993177d8c6d125a5dc
Lyncoln Campello <lyncolncamp@gmail.com>
2025-01-28 20:27:57 -0300

    criando sobre nos

---
+++ src/components/sobrenos/SobreNos.tsx
@@ -0,0 +1,30 @@
+const SobreNos = () => {
+    return (
+      <section className="flex flex-col items-center text-center bg-[#e2ddd9] py-20 px-8">
+        
+        <div className="mb-8">
+          <img src="/src/assets/about-us-svgrepo-com.svg" alt="Sobre Nós" className="w-20 h-20" />
+        </div>
+  
+    
+        <h2 className="text-5xl font-bold text-gray-800 mb-6">
+          Sobre Nós
+        </h2>
+  
+        <p className="max-w-3xl text-2xl text-gray-700 leading-relaxed">
+        Nosso site fitness nasceu para tornar a vida saudável acessível a todos.  
+        Acreditamos que a mudança começa pelo movimento, e nosso objetivo é  
+        fornecer treinos práticos e acessíveis para qualquer pessoa, em qualquer lugar.  
+      </p>
+
+      <p className="max-w-3xl text-2xl text-gray-700 leading-relaxed mt-6">
+        Não importa se você está começando ou já tem experiência: aqui, você encontra  
+        motivação e suporte para evoluir e alcançar seus objetivos.  
+        
+      </p>
+      </section>
+    );
+  };
+  
+  export default SobreNos;
+  
