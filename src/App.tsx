2f4ffce27cf754d8ae82d991b882adb760526895
Lyncoln Campello <lyncolncamp@gmail.com>
2025-01-28 21:19:34 -0300

    configuração inicial do footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,18 @@ import React from 'react'
 import Home from './pages/home/Home'
 import Navbar from './components/navbar/Navbar'
 import Services from './components/services/Services'   
-import SobreNos from './components/sobrenos/Sobrenos'     
+import SobreNos from './components/sobrenos/SobreNos'    
+import Footer from './components/footer/Footer' 
 
 
 function App() {
     return (
       <>
         <Navbar/>
-        <Home />
-        <Services />
-        <SobreNos />
+        <Home/>
+        <Services/>
+        <SobreNos/>
+        <Footer/>
       </>
     );
 }
